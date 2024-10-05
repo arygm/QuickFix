728be9bda0caa0d4a0a943c40894838ca098184d
JitterKiller <a1337player@gmail.com>
2024-10-05 18:40:54 +0200

    chore(ci): set up SonarCloud integration
    - Configured SonarCloud properties in `build.gradle.kts` for project analysis.
    - Added SonarCloud scan job to the CI workflow in `.github/workflows/CI.yaml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,9 +107,9 @@ android {
 
 sonar {
     properties {
-        property("sonar.projectKey", "gf_android-sample")
-        property("sonar.projectName", "Android-Sample")
-        property("sonar.organization", "gabrielfleischer")
+        property("sonar.projectKey", "arygm_QuickFix")
+        property("sonar.projectName", "QuickFix")
+        property("sonar.organization", "quickfix")
         property("sonar.host.url", "https://sonarcloud.io")
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
         property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugunitTest/")
