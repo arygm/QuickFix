abb015dadd6951c04aba69563b245785f57e5df6
JitterKiller <a1337player@gmail.com>
2024-10-05 19:01:24 +0200

    test: update ExampleInstrumentedTest to target SecondActivity
    - Modified `ExampleInstrumentedTest.kt` to test `SecondActivity` instead of `MainActivity`.

--- app/src/androidTest/java/com/arygm/quickfix/screen/SecondScreen.kt
+++ app/src/androidTest/java/com/arygm/quickfix/screen/SecondScreen.kt
@@ -5,10 +5,10 @@ import com.arygm.quickfix.resources.C
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
 
-class MainScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
-    ComposeScreen<MainScreen>(
+class SecondScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<SecondScreen>(
         semanticsProvider = semanticsProvider,
-        viewBuilderAction = { hasTestTag(C.Tag.main_screen_container) }) {
+        viewBuilderAction = { hasTestTag(C.Tag.second_screen_container) }) {
 
-  val simpleText: KNode = child { hasTestTag(C.Tag.greeting) }
+  val simpleText: KNode = child { hasTestTag(C.Tag.greeting_robo) }
 }
