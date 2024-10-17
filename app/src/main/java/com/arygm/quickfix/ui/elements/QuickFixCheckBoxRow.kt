b47fd74c729104ceb4462a2a54592dc552bcb802
JitterKiller <a1337player@gmail.com>
2024-10-17 16:00:11 +0200

    style(ui): update QuickFixCheckBoxRow to support a second clickable label
    - Enhanced QuickFixCheckBoxRow to display and handle a second clickable label

--- app/src/main/java/com/arygm/quickfix/ui/elements/QuickFixCheckBoxRow.kt
+++ app/src/main/java/com/arygm/quickfix/ui/elements/QuickFixCheckBoxRow.kt
@@ -1,9 +1,12 @@
 package com.arygm.quickfix.ui.elements
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
@@ -21,35 +24,67 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun QuickFixCheckBoxRow(
+    modifier: Modifier = Modifier,
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit,
     label: String = "",
     underlinedText: String = "",
     onUnderlinedTextClick: () -> Unit,
+    labelBis: String = "",
+    underlinedTextBis: String = "",
+    onUnderlinedTextClickBis: (() -> Unit)? = null,
     colorScheme: ColorScheme = MaterialTheme.colorScheme
 ) {
-  Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-    Checkbox(
-        checked = checked,
-        onCheckedChange = onCheckedChange,
-        colors =
-            CheckboxDefaults.colors(
-                checkedColor = colorScheme.primary,
-                uncheckedColor = colorScheme.tertiary,
-                checkmarkColor = Color.Transparent),
-        modifier = Modifier.size(24.dp).testTag("checkbox"))
-    Spacer(modifier = Modifier.width(10.dp))
-    Text(
-        label,
-        style = MaterialTheme.typography.labelSmall,
-        color = Color(0xFFC0C0C0),
-        modifier = Modifier.testTag("checkBoxInfo"))
-    Spacer(modifier = Modifier.width(4.dp))
-    Text(
-        text = underlinedText,
-        style = MaterialTheme.typography.labelSmall,
-        color = colorScheme.primary,
-        textDecoration = TextDecoration.Underline,
-        modifier = Modifier.clickable(onClick = onUnderlinedTextClick).testTag("clickableLink"))
+  Column(
+      modifier = modifier.fillMaxWidth().padding(end = 3.dp),
+  ) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.testTag("checkBoxRow")) {
+          Checkbox(
+              checked = checked,
+              onCheckedChange = onCheckedChange,
+              colors =
+                  CheckboxDefaults.colors(
+                      checkedColor = colorScheme.primary,
+                      uncheckedColor = colorScheme.tertiary,
+                      checkmarkColor = Color.Transparent),
+              modifier = modifier.size(10.dp).testTag("checkbox"))
+          Spacer(modifier = modifier.width(1.dp))
+          Text(
+              label,
+              style = MaterialTheme.typography.headlineSmall,
+              color = colorScheme.onSurface,
+              modifier = modifier.testTag("checkBoxInfo"))
+          Text(
+              text = underlinedText,
+              style = MaterialTheme.typography.headlineSmall,
+              color = colorScheme.primary,
+              textDecoration = TextDecoration.Underline,
+              maxLines = 1,
+              modifier =
+              modifier.clickable(onClick = onUnderlinedTextClick).testTag("clickableLink"))
+        }
+    Row(
+        modifier = modifier.padding(start = 22.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+          Text(
+              labelBis,
+              style = MaterialTheme.typography.headlineSmall,
+              color = colorScheme.onSurface,
+              modifier = modifier.testTag("checkBoxInfoBis"))
+          Text(
+              text = underlinedTextBis,
+              style = MaterialTheme.typography.headlineSmall,
+              color = colorScheme.primary,
+              textDecoration = TextDecoration.Underline,
+              modifier =
+              modifier.clickable(
+                          onClick = {
+                            if (onUnderlinedTextClickBis != null) onUnderlinedTextClickBis()
+                          })
+                      .testTag("clickableLinkBis"))
+        }
+    Spacer(modifier = modifier.height(10.dp))
   }
 }
