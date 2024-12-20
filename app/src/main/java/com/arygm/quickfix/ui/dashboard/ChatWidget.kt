775adaa0f72f16d85520aa8bdec2ccadcd18b7c3
Mehdi Alaoui <mehdi.alaoui@epfl.ch>
2024-12-20 08:09:59 +0100

    bug: fix a bug with dashboard screen

--- app/src/main/java/com/arygm/quickfix/ui/dashboard/ChatWidget.kt
+++ app/src/main/java/com/arygm/quickfix/ui/dashboard/ChatWidget.kt
@@ -236,7 +236,8 @@ fun ChatItem(
                       (chat.chatStatus == ChatStatus.WAITING_FOR_RESPONSE ||
                           chat.chatStatus == ChatStatus.GETTING_SUGGESTIONS)) {
                     "Await confirmation from ${workerProfile.displayName}"
-                  } else chat.messages.last().content, // Removed leading comma for clarity
+                  } else if (chat.messages.isNotEmpty()) chat.messages.last().content
+                  else "", // Removed leading comma for clarity
               modifier =
                   Modifier.testTag(
                       if (chat.messages.isEmpty()) "No messages"
